21a53fb3cfcf35527591f458de5da25388f60b6b
Natarajan <nataraj.itsolution@gmail.com>
2022-12-22 22:10:10 +0530

    Login

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,24 @@
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
 const Home = () => {
-    return ( 
+    const usenavigate = useNavigate();
+    useEffect(() => {
+     let username=sessionStorage.getItem('username');
+     if(username==='' || username ===null){
+        usenavigate('/login');
+     }
+    }, []);
+
+    return (
         <div>
-            <h1>Welcome to Nihira Techiees</h1>
+            <div className="header">
+                <Link to={'/'}>Home</Link>
+                <Link style={{float:'right'}} to={'/login'}>Logout</Link>
+            </div>
+            <h1 className="text-center">Welcome to Nihira Techiees</h1>
         </div>
-     );
+    );
 }
- 
+
 export default Home;
--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,81 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
 const Login = () => {
-    return ( 
-        <div>
-            <h1>Login</h1>
+    const [username, usernameupdate] = useState('');
+    const [password, passwordupdate] = useState('');
+
+    const usenavigate=useNavigate();
+
+    useEffect(()=>{
+sessionStorage.clear();
+    },[]);
+
+    const ProceedLogin = (e) => {
+        e.preventDefault();
+        if (validate()) {
+            ///implentation
+            // console.log('proceed');
+            fetch("http://localhost:8000/user/" + username).then((res) => {
+                return res.json();
+            }).then((resp) => {
+                //console.log(resp)
+                if (Object.keys(resp).length === 0) {
+                    toast.error('Please Enter valid username');
+                } else {
+                    if (resp.password === password) {
+                        toast.success('Success');
+                        sessionStorage.setItem('username',username);
+                        usenavigate('/')
+                    }else{
+                        toast.error('Please Enter valid credentials');
+                    }
+                }
+            }).catch((err) => {
+                toast.error('Login Failed due to :' + err.message);
+            });
+        }
+    }
+    const validate = () => {
+        let result = true;
+        if (username === '' || username === null) {
+            result = false;
+            toast.warning('Please Enter Username');
+        }
+        if (password === '' || password === null) {
+            result = false;
+            toast.warning('Please Enter Password');
+        }
+        return result;
+    }
+    return (
+        <div className="row">
+            <div className="offset-lg-3 col-lg-6" style={{ marginTop: '100px' }}>
+                <form onSubmit={ProceedLogin} className="container">
+                    <div className="card">
+                        <div className="card-header">
+                            <h2>User Login</h2>
+                        </div>
+                        <div className="card-body">
+                            <div className="form-group">
+                                <label>User Name <span className="errmsg">*</span></label>
+                                <input value={username} onChange={e => usernameupdate(e.target.value)} className="form-control"></input>
+                            </div>
+                            <div className="form-group">
+                                <label>Password <span className="errmsg">*</span></label>
+                                <input type="password" value={password} onChange={e => passwordupdate(e.target.value)} className="form-control"></input>
+                            </div>
+                        </div>
+                        <div className="card-footer">
+                            <button type="submit" className="btn btn-primary">Login</button> |
+                            <Link className="btn btn-success" to={'/register'}>New User</Link>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default Login;
