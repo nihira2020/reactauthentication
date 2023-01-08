b8cd6ea157dcc0a8ef8c3549bb172fef2aaa2c7b
Natarajan <nataraj.itsolution@gmail.com>
2023-01-08 09:20:41 +0530

    JWT Token handling

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,61 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const Home = () => {
     const usenavigate = useNavigate();
+    const [customerlist, listupdate] = useState(null);
     useEffect(() => {
-     let username=sessionStorage.getItem('username');
-     if(username==='' || username ===null){
-        usenavigate('/login');
-     }
+        let username = sessionStorage.getItem('username');
+        if (username === '' || username === null) {
+            usenavigate('/login');
+        }
+
+        let jwttoken = sessionStorage.getItem('jwttoken');
+        fetch("https://localhost:44308/Customer", {
+            headers: {
+                'Authorization': 'bearer ' + jwttoken
+            }
+        }).then((res) => {
+            return res.json();
+        }).then((resp) => {
+            listupdate(resp);
+        }).catch((err) => {
+            console.log(err.messsage)
+        });
+
     }, []);
 
     return (
         <div>
             <div className="header">
                 <Link to={'/'}>Home</Link>
-                <Link style={{float:'right'}} to={'/login'}>Logout</Link>
+                <Link style={{ float: 'right' }} to={'/login'}>Logout</Link>
             </div>
             <h1 className="text-center">Welcome to Nihira Techiees</h1>
+            <table className="table table-bordered">
+                <thead>
+                    <tr>
+                        <td>Code</td>
+                        <td>Name</td>
+                        <td>Email</td>
+                        <td>Credit Limit</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {customerlist &&
+                        customerlist.map(item => (
+                            <tr key={item.id}>
+        <td>{item.id}</td>
+        <td>{item.name}</td>
+        <td>{item.email}</td>
+        <td>{item.creditLimit}</td>
+                            </tr>
+
+                        ))
+                    }
+                </tbody>
+
+            </table>
         </div>
     );
 }
--- src/Login.js
+++ src/Login.js
@@ -37,6 +37,46 @@ sessionStorage.clear();
             });
         }
     }
+
+    const ProceedLoginusingAPI = (e) => {
+        e.preventDefault();
+        if (validate()) {
+            ///implentation
+            // console.log('proceed');
+            let inputobj={"username": username,
+            "password": password};
+            fetch("https://localhost:44308/User/Authenticate",{
+                method:'POST',
+                headers:{'content-type':'application/json'},
+                body:JSON.stringify(inputobj)
+            }).then((res) => {
+                return res.json();
+            }).then((resp) => {
+                console.log(resp)
+                if (Object.keys(resp).length === 0) {
+                    toast.error('Login failed, invalid credentials');
+                }else{
+                     toast.success('Success');
+                     sessionStorage.setItem('username',username);
+                     sessionStorage.setItem('jwttoken',resp.jwtToken);
+                   usenavigate('/')
+                }
+                // if (Object.keys(resp).length === 0) {
+                //     toast.error('Please Enter valid username');
+                // } else {
+                //     if (resp.password === password) {
+                //         toast.success('Success');
+                //         sessionStorage.setItem('username',username);
+                //         usenavigate('/')
+                //     }else{
+                //         toast.error('Please Enter valid credentials');
+                //     }
+                // }
+            }).catch((err) => {
+                toast.error('Login Failed due to :' + err.message);
+            });
+        }
+    }
     const validate = () => {
         let result = true;
         if (username === '' || username === null) {
@@ -52,7 +92,7 @@ sessionStorage.clear();
     return (
         <div className="row">
             <div className="offset-lg-3 col-lg-6" style={{ marginTop: '100px' }}>
-                <form onSubmit={ProceedLogin} className="container">
+                <form onSubmit={ProceedLoginusingAPI} className="container">
                     <div className="card">
                         <div className="card-header">
                             <h2>User Login</h2>
